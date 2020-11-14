710ef2d204c2c4dd39a162573601199ea4474656
Igor P-V <ispogiba@gmail.com>
2020-11-14 13:25:58 +0300

    Initial Commit

---
+++ My Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Movie
+//
+//  Created by Elena Igumenova on 14.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
